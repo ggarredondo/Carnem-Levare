142a51008ffa4331840f1e7b5bfd669a93496511
Rullow <rullowgl@gmail.com>
2022-09-19 20:24:46 +0200

    Implemented crouch controls and animations

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -6,24 +6,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Animator anim;
-    private CharacterController controller;
 
     private Vector2 left_stick;
-
     private Vector3 initialPlayerPosition;
 
+    private float horizontal = 0f, vertical = 0f;
+    private const float vertical_min = -1f, vertical_max = 0f;
+
     [Header("Animation Parameters")]
-    [Range(-1f, 1f)] public float load = 0f;
     [Range(-2f, 2f)] public float speed = 1f;
+    [Range(-1f, 1f)] public float load = 0f;
 
     [Header("Movement Parameters")]
-    [Range(0f, 10f)] public float movementSpeed = 0f;
-    [Range(0f, 2f)] public float playerMovementDetectzone;
+    [Range(0f, 10f)] public float movementSpeed = 1f;
+    [Range(0f, 10f)] public float returnSpeed = 1f;
+
+    [Range(0f, 10f)] public float translateSpeed = 1f;
+    [Range(0f, 2f)] public float playerTranslateDetectzone;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
-        controller = GetComponent<CharacterController>();
     }
 
     private void Start()
@@ -35,7 +38,6 @@ public class PlayerController : MonoBehaviour
     {
         Animation();
         PlayerMovement();
-        PlayerComeBack();
     }
 
     /// <summary>
@@ -45,6 +47,8 @@ public class PlayerController : MonoBehaviour
     {
         anim.SetFloat("load", load);
         anim.SetFloat("speed", speed);
+        anim.SetFloat("vertical", vertical);
+        anim.SetFloat("horizontal", horizontal);
     }
 
     //***CONTROLS***
@@ -60,30 +64,44 @@ public class PlayerController : MonoBehaviour
     //***MOVEMENT_FUNCTIONS***
 
     /// <summary>
-    /// Make the player move with the left stick values
+    /// Modifies animator parameters to move the player through animations
     /// </summary>
     private void PlayerMovement()
     {
-        Vector3 movement = new Vector3(left_stick.x  * movementSpeed, 0, 0);
+        // If left stick is pushed, character moves according to the stick
+        if (left_stick.magnitude > 0f)
+            vertical += left_stick.y * Time.deltaTime * movementSpeed;
+
+        else // If left stick isn't being pushed, character returns to center
+            vertical += Time.deltaTime * returnSpeed;
+        vertical = Mathf.Clamp(vertical, vertical_min, vertical_max);
+    }
+
+    /// <summary>
+    /// Translates the player actor from left to right according to left stick
+    /// </summary>
+    private void PlayerTranslate()
+    {
+        Vector3 movement = new Vector3(left_stick.x  * translateSpeed, 0, 0);
         transform.Translate(movement * Time.deltaTime);
     }
 
     /// <summary>
-    /// The player and camera come back to the initial position after user move it
+    /// The player and camera come back to the initial position after user moves it
     /// </summary>
-    private void PlayerComeBack()
+    private void PlayerTranslateComeback()
     {
-        //Move the player to the initial point if the player is not moving
-        //There must be a detectzone because we cant reach the initial point exactly
-        if (NearVectors(transform.position, initialPlayerPosition, playerMovementDetectzone) && left_stick == new Vector2(0, 0))
+        //Translates the player to the initial point if the player is not moving
+        //There must be a detect zone because we can't reach the initial point exactly
+        if (NearVectors(transform.position, initialPlayerPosition, playerTranslateDetectzone) && left_stick == new Vector2(0, 0))
         {
-            Vector3 playerMoveDir = (transform.position - initialPlayerPosition) * movementSpeed;
+            Vector3 playerMoveDir = (transform.position - initialPlayerPosition) * translateSpeed;
             transform.Translate(playerMoveDir *  Time.deltaTime);
         }
     }
 
     /// <summary>
-    /// Check if the distance between vectors is greather than a value
+    /// Check if the distance between vectors is greater than a value
     /// </summary>
     /// <param name="actualPosition">The actual point</param>
     /// <param name="target">The target point that defines the distance</param>
