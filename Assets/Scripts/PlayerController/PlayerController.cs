c5add1c59e5d8a179bd9c6ace802418775d92677
Rullow <rullowgl@gmail.com>
2022-09-28 13:05:06 +0200

    Implemented blocking

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     private Animator anim;
     private Vector2 movement_value, direction;
-    private float left_jab_value, right_jab_value, left_special_value, right_special_value;
+    private float left_jab_value, right_jab_value, left_special_value, right_special_value, block_value;
     private bool is_attacking;
 
     public Transform TargetEnemy;
@@ -40,13 +40,14 @@ public class PlayerController : MonoBehaviour
     { 
         movement_value = context.ReadValue<Vector2>();
         movement_value.y = Mathf.Clamp(movement_value.y, -1f, 0f); // -1 is crouching, 0 is standing. Doesn't make sense to consider 1 as a value.
-        Debug.Log(movement_value);
+        Debug.Log(movement_value); // <----- DELETE
     }
 
     public void LeftJab(InputAction.CallbackContext context) { left_jab_value = context.ReadValue<float>(); }
     public void RightJab(InputAction.CallbackContext context) { right_jab_value = context.ReadValue<float>(); }
     public void LeftSpecial(InputAction.CallbackContext context) { left_special_value = context.ReadValue<float>(); }
     public void RightSpecial(InputAction.CallbackContext context) { right_special_value = context.ReadValue<float>(); }
+    public void Block (InputAction.CallbackContext context) { block_value = context.ReadValue<float>();  }
 
     //***ANIMATION***
 
@@ -56,7 +57,7 @@ public class PlayerController : MonoBehaviour
     private void SetAnimationParameters()
     {
         is_attacking = anim.GetCurrentAnimatorStateInfo(0).IsTag("Attacking") && !anim.IsInTransition(0);
-        anim.SetBool("is_attacking", is_attacking);
+        anim.SetBool("cant_attack", is_attacking || block_value > 0f);
         anim.SetFloat("load", load);
         anim.SetFloat("speed", speed);
 
@@ -72,15 +73,15 @@ public class PlayerController : MonoBehaviour
         anim.SetFloat("right_jab", right_jab_value);
         anim.SetFloat("left_special", left_special_value);
         anim.SetFloat("right_special", right_special_value);
+
+        // OTHER
+        anim.SetFloat("block", block_value);
     }
 
     //***PUBLIC METHODS***
 
     public Animator getAnimator { get { return anim; } }
-    public float getLeftJabValue { get { return left_jab_value; } }
-    public float getRightJabValue { get { return right_jab_value; } }
-    public float getLeftSpecialValue { get { return left_special_value; } }
-    public float getRightSpecialValue { get { return right_special_value; } }
+    public Vector2 getDirection { get { return direction; } }
 
     public bool isWalking { get { return direction.x != 0f && !is_attacking; } }
 }
