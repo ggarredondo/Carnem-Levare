a736f0aa22c9b2c5c5c26a45b39ae8e27265845b
Rullow <rullowgl@gmail.com>
2022-11-21 18:32:15 +0100

    Removed useless attributes

--- Assets/Scripts/Character/Move.cs
+++ Assets/Scripts/Character/Move.cs
@@ -3,15 +3,7 @@ using UnityEngine;
 public enum Power: uint
 {
     Light = 0,
-    Medium = 1,
-    Strong = 2
-}
-
-public enum Side: int
-{
-    Left = -1,
-    Center = 0,
-    Right = 1
+    Strong = 1
 }
 
 /// <summary>
@@ -33,11 +25,6 @@ public class Move : MonoBehaviour
     /// Move's power, classified by Light, Medium and Strong. Used to choose between hurt animations.
     /// </summary>
     public Power power;
-
-    /// <summary>
-    /// Move's direction, from left to right (including center). Used to choose between hurt animations.
-    /// </summary>
-    public Side side;
 
     /// <summary>
     /// The damage it deals to the opponent's stamina, if it hits.
