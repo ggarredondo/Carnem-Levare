6cdd14dcaf1c4cd6c76226fa24412f39a442c0a0
Alejandro Cruz Lemos <alexcruzlemos12082000@gmail.com>
2023-02-17 14:56:54 +0100

    Dolly Zoom improvement
    Now the field of view is a variation and not an interval

--- Assets/Scripts/Camera/CameraMovements/DollyZoom.cs
+++ Assets/Scripts/Camera/CameraMovements/DollyZoom.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class DollyZoom : CameraMovement
 {
-    public Tuple<float> fieldOfView;
+    public float fieldOfViewVariation;
     public Vector3 offsetVariation;
 
     private float reduceFOV, reduceZoom;
     [HideInInspector] public Tuple<Vector3> zoomPositions;
+    [HideInInspector] public Tuple<float> fieldOfView;
     private CinemachineTransposer transposer;
 
     public override void Awake()
@@ -25,6 +26,9 @@ public class DollyZoom : CameraMovement
     {
         zoomPositions.Item1 = transposer.m_FollowOffset;
         zoomPositions.Item2 = zoomPositions.Item1 + offsetVariation;
+
+        fieldOfView.Item1 = vcam.m_Lens.FieldOfView;
+        fieldOfView.Item2 = fieldOfView.Item1 + fieldOfViewVariation;
     }
 
     public override void ApplyMove(bool condition)
