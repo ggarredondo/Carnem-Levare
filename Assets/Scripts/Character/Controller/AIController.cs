3762b8325dd85d139bc577ed47716d35299fb620
Rullow <rullowgl@gmail.com>
2023-05-11 08:22:18 +0200

    Added debug move buttons to AI script

--- Assets/Scripts/Character/Controller/AIController.cs
+++ Assets/Scripts/Character/Controller/AIController.cs
@@ -4,11 +4,17 @@ public class AIController : Controller
 {
     [SerializeField] [Range(-1f, 1f)] private float horizontal, vertical;
     [SerializeField] private bool block = false;
+    [SerializeField] private bool move0, move1, move2, move3;
 
     private void OnValidate()
     {
         movementVector.x = horizontal;
         movementVector.y = vertical;
         DoBlock(block);
+
+        if (move0) { move0 = false; DoMove(0); }
+        if (move1) { move1 = false; DoMove(1); }
+        if (move2) { move2 = false; DoMove(2); }
+        if (move3) { move3 = false; DoMove(3); }
     }
 }
