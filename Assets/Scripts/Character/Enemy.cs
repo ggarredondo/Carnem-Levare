24cf7bec2b8b7957587836847b566ecd0a188e3b
Guillermo García <55685909+ggarredondo@users.noreply.github.com>
2023-06-12 13:09:50 +0200

    Added enemy drops to Enemy class

--- Assets/Scripts/Character/Enemy.cs
+++ Assets/Scripts/Character/Enemy.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : Character
 {
     private Character player;
+    [SerializeField] private List<Move> enemyDrops;
 
     protected override void Awake()
     {
@@ -16,4 +18,6 @@ public class Enemy : Character
         base.Start();
         ((AIController) controller).Reference(characterStats, player.CharacterStats, stateMachine, player.StateMachine);
     }
+
+    public ref readonly List<Move> EnemyDrops => ref enemyDrops;
 }
